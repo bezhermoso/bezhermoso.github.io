66f6fd1da777888b5ef6f12e24b66ca374723506
Bez Hermoso <bezalelhermoso@gmail.com>
2016-01-18 14:49:12 -0800

    Favicon

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,10 +246,10 @@ module.exports = function (grunt) {
             'fonts/**/*',
             'keep/**/*',
             // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}'
+            '!**/_*{,/**}',
             // Explicitly add any files your site needs for distribution here.
             //'_bower_components/jquery/jquery.min.js',
-            //'favicon.ico',
+            'favicon.png',
             //'apple-touch*.png'
           ],
           dest: '<%= yeoman.dist %>'

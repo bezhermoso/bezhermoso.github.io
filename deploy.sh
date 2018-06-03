81ac9d1b27d738fff156c29300acb0b5d1cfea83
Bezalel Hermoso <bez@weebly.com>
2018-06-03 06:00:56 -0700

    Make Gulp command customizable

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gulp build && cd dist && git add * && git commit -S && git push origin master -f
+GULP_COMMAND=${GULP_COMMAND:-yarn run gulp}
+$GULP_COMMAND build && cd dist && git add * && git commit -S && git push origin master -f

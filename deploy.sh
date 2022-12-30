e2ff34906425b2adcbc382abb0ea3ca92053ee52
Bez Hermoso <bezalel@squareup.com>
2022-12-29 16:25:50 -0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BUILD_CMD=${BUILD_CMD:-yarn run gulp build}
+BUILD_CMD=${BUILD_CMD:-./node_modules/.bin/gulp build}
 $BUILD_CMD \
   && git add -A dist \
-  && fortune -s | git commit -S -m - \
+  && fortune -s | git commit -m - \
   && git subtree push --prefix=dist origin master
